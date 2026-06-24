0c8fb52273a10b46ab481b49c87aaa11f94c5e2a
NoorAzam-1 <azamnoor698@gmail.com>
2026-06-24 17:14:46 +0530

    initialize project with basic structure and configuration files

---
+++ Node/index.js
@@ -0,0 +1,19 @@
+console.log("hello");
+
+// path Module
+// import path from "path";
+// console.log(path.basename(__filename));
+
+
+// os Module 
+import os from "os";
+console.log(os.platform());
+console.log(os.hostname());
+
+// Sever Creation
+// import express from "express";
+// const app = express();
+
+// app.listen(5000, () => {
+//   console.log("Server Running");
+// });
