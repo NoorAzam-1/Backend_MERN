16584c3f5e18a910f27fa515031ff548e191f08b
NoorAzam-1 <azamnoor698@gmail.com>
2026-07-22 16:17:13 +0530

    refactor index.js: clean up commented code and improve file reading logic

--- Node/index.js
+++ Node/index.js
@@ -1,19 +1,44 @@
+const fs = require("fs");
 console.log("hello");
 
-// path Module
-// import path from "path";
-// console.log(path.basename(__filename));
+// const math = require("./math");
+// console.log(math.add(12,3));
+// console.log(math.subtract(12,3));
+
+// FS Module
+
+// fs.writeFileSync("createFileUsingFs.txt", "hello Noor");
 
 
-// os Module 
-import os from "os";
-console.log(os.platform());
-console.log(os.hostname());
+fs.readFile("createFileUsingFs.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log("Error :", err);
+    return;
+  }
+  console.log("3. File ka data aa gaya:", data);
+});
 
-// Sever Creation
-// import express from "express";
+const readedData = fs.readFileSync("createFileUsingFs.txt", "utf-8");
+console.log("readed data", readedData);
+
+
+
+
+
+
+
+
+// const express = require('express');
 // const app = express();
 
+// app.get('/', (req, res) => {
+//   res.send('Welcome to My Backend Server!');
+// });
+
+// app.get('/about', (req, res) => {
+//   res.send('Welcome to My About Page!');
+// });
+
 // app.listen(5000, () => {
-//   console.log("Server Running");
+//   console.log(`Server running on http://localhost:5000`);
 // });
