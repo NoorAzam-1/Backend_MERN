dff46b89c53f555e5169523339fdf82f4ef57c61
NoorAzam-1 <azamnoor698@gmail.com>
2026-07-22 16:45:47 +0530

    add fs.js: implement file creation and reading functionality

---
+++ Node/fs.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+// Created File
+fs.writeFileSync("createFileUsingFs.txt", "hello Noor Are you Free");
+
+// Asynchronous read file
+fs.readFile("createFileUsingFs.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log("Error :", err);
+    return;
+  }
+  console.log("3. File ka data aa gaya:", data);
+});
+
+// Synchronous Read File
+const readedData = fs.readFileSync("createFileUsingFs.txt", "utf-8");
+console.log("readed data", readedData);
+
+
+
+
