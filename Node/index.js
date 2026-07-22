833cc3c7c3fd52a803cda89da26f13410cce1f19
NoorAzam-1 <azamnoor698@gmail.com>
2026-07-22 16:45:54 +0530

    remove fs module usage and related file operations from index.js

--- Node/index.js
+++ Node/index.js
@@ -1,28 +1,8 @@
-const fs = require("fs");
 console.log("hello");
 
 // const math = require("./math");
 // console.log(math.add(12,3));
 // console.log(math.subtract(12,3));
-
-// FS Module
-
-// fs.writeFileSync("createFileUsingFs.txt", "hello Noor");
-
-
-fs.readFile("createFileUsingFs.txt", "utf-8", (err, data) => {
-  if (err) {
-    console.log("Error :", err);
-    return;
-  }
-  console.log("3. File ka data aa gaya:", data);
-});
-
-const readedData = fs.readFileSync("createFileUsingFs.txt", "utf-8");
-console.log("readed data", readedData);
-
-
-
 
 
 
