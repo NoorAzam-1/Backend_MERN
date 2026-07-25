0f6bd4330a0883348ec23385bf6981a5018a7d9d
NoorAzam-1 <azamnoor698@gmail.com>
2026-07-25 18:07:37 +0530

    refactor fs.js: comment out unused file operations for improved clarity

--- Node/fs.js
+++ Node/fs.js
@@ -1,25 +1,24 @@
 import fs from "fs";
 
-fs.writeFileSync("createFileUsingFs.txt", "1.hello Noor Are you available");
+// fs.writeFileSync("createFileUsingFs.txt", "1.hello Noor Are you available");
 
 
-fs.readFile("createFileUsingFs.txt", "utf-8", (err, data) => {
-  if (err) {
-    console.log("Error :", err);
-    return;
+fs.writeFile("/Node/logs/2026/july/createFileUsingFs.txt","nested file", (err)=>{
+  if(err){
+    console.log(err);
+    return
   }
-  console.log("3. Data of File:", data);
-});
+})
 
-const readedData = fs.readFileSync("createFileUsingFs.txt", "utf-8");
-console.log("3.readed data", readedData);
-
+// const readedData = fs.readFileSync("createFileUsingFs.txt", "utf-8");
+// console.log("3.readed data", readedData);
 
 
 
-fs.mkdir('my_logs', (err) => {
-  if (err) console.log("Folder banane mein error:", err);
-  else console.log("Folder ban gaya!");
-});
 
-fs.mkdirSync('logs/2026/july', { recursive: true });
+// fs.mkdir('my_logs', (err) => {
+//   if (err) console.log("Folder banane mein error:", err);
+//   else console.log("Folder ban gaya!");
+// });
+
+// fs.mkdirSync('logs/2026/july', { recursive: true });
