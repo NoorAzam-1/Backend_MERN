893d954daec569ec67be2c6fb4d0f593505ae861
NoorAzam-1 <180100808+NoorAzam-1@users.noreply.github.com>
2026-07-29 18:14:19 +0530

    refactor fs.js: comment out additional file operations for improved clarity

--- Node/fs.js
+++ Node/fs.js
@@ -1,45 +1,14 @@
 import fs from "fs";
-console.log("hello 1");
 
-// fs.writeFileSync("notes.txt","hello")
-
-
-
-
-// fs.appendFile("notes.txt", "const num = 15", (err)=>{
-//     if(err){
-//         console.log("file error",err)
-//     }
-// })
-
-fs.unlink("notes.txt",(err)=>{
-    console.log("error",err)
-})
+// fs.writeFileSync("notes.txt","hello");
 
 // fs.appendFileSync("notes.txt", "new data added")
-
-
 
 // fs.appendFile('notes.txt', '\n Third Note: Server restarted.', (err) => {
 //   if (err) console.log(err);
 //   else console.log("New Notes appended!");
 // });
 
-// const readedData = fs.readFileSync("notes.txt", "utf-8");
-// console.log("3.readed data", readedData);
-
-// const readedDataAsync = fs.readFile("notes.txt", "utf-8", (err, data)=>{
-//   if(err){
-//     console.log(err)
-//     return
-//   }
-//   console.log(data)
-// })
-
-// fs.readdir("./", { recursive: true }, (err, files) => {
-//   if (err) console.log(err);
-//   else console.log("File lists:", files);
-// });
 
 // fs.unlink('notes.txt', (err) => {
 //   if (err) {
@@ -49,9 +18,43 @@ fs.unlink("notes.txt",(err)=>{
 //   console.log("File deleted successfully!");
 // });
 
+// const readedData = fs.readFileSync("notes.txt", "utf-8");
+// console.log("3.readed data ", readedData);
+
+// const readedDataAsync = fs.readFile("notes.txt", "utf-8", (err, data)=>{
+//   if(err){
+//     console.log(err)
+//     return
+//   }
+//   console.log(data)
+// })
+
+
+
 // fs.mkdirSync("logs", { recursive: true });
 
 // fs.mkdir("my_logs", (err) => {
 //   if (err) console.log("Folder error:", err);
 //   else console.log("Folder created!");
 // });
+
+
+
+// fs.readdir("./", { recursive: true }, (err, files) => {
+//   if (err) console.log(err);
+//   else console.log("File lists:", files);
+// });
+
+
+
+// fs.renameSync('logs1', 'archive_logs');
+//   console.log("Folder rename successfully!");
+
+
+// fs.rmdir('my_logs', (err) => {
+//   if (err) console.log(err);
+//   else console.log("Folder deleteted successfully!");
+// });
+
+// fs.rmSync('logs', { recursive: true, force: true });
+
